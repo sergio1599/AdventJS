9e8d2fc7340fb44d76a23324394fd3c5f9295c02
sergio1599 <sergioq15@hotmail.com>
2021-12-03 22:06:56 -0500

    finish third exercise

---
+++ 03-grinch/isValid.js
@@ -0,0 +1,33 @@
+
+//const cadena = "bici coche (balón) bici coche peluche"; 
+const cadena = "(muñeca) consola bici"; 
+//const cadena = "bici coche (balón bici coche"; 
+
+
+/*function isValid(letter){
+    
+    //const reg = /[\(\){ \}\[\]]+/g; 
+    const reg = /[\[\]\{\}]/i 
+
+    if(!letter.search(reg)){
+        return true
+    }
+
+    return false
+}*/
+
+
+function isValid(letter){
+    
+    if( letter.includes("[") || letter.includes("]") || letter.includes("{") || letter.includes("}") || letter.includes("()")  ){
+        return false; 
+    }else if( letter.includes("(") && letter.includes(")") ){
+        return true
+    }
+    return false
+   
+}
+
+const validation = isValid(cadena); 
+
+console.log(validation)
