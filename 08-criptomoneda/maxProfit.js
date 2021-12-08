3d9c9bea7ade428ab647e51085baeb2d73fbdff2
sergio1599 <sergioq15@hotmail.com>
2021-12-08 12:43:30 -0500

    complete the eighth challege

---
+++ 08-criptomoneda/maxProfit.js
@@ -0,0 +1,22 @@
+
+const pricesBtc = [18, 15, 12, 11, 9, 7];
+
+
+function maxProfit(prices) {
+
+
+    const index = prices.length;
+    const purchase = prices.slice(0, index / 2);
+    const sale = prices.slice(index / 2, index);
+    const min = Math.min(...purchase);
+    const max = Math.max(...sale)
+
+    let resp = max <= min ? -1 : max - min;
+
+    return resp;
+
+}
+
+const res = maxProfit(pricesBtc);
+
+console.log(res)
